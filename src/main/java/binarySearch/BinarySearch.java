cae8aff72f7e35d7865e5be9f4b48ad7dffd0ec6
adam sarenski <asarenski@zipcar.com>
2017-12-26 16:56:57 -0500

    added binary search

---
+++ src/main/java/binarySearch/BinarySearch.java
@@ -0,0 +1,23 @@
+package binarySearch;
+
+import java.util.List;
+
+public class BinarySearch {
+    public static Boolean isIncludedInList(Integer desiredNum, List<Integer> list) {
+        Integer low = 0;
+        Integer high = list.size() - 1;
+
+        while (high >= low) {
+            final Integer mid = (high + low) / 2;
+            if (list.get(mid) == desiredNum) {
+                return true;
+            } else if (list.get(mid) > desiredNum) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/binarySearch/testBinarySearch.java
@@ -0,0 +1,28 @@
+package binarySearch;
+
+import com.google.common.collect.Lists;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class testBinarySearch {
+    final private List<Integer> testList = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    @Test
+    public void isIncludedInListTrue() throws Exception {
+        testList.forEach((integer) -> {
+            Assert.assertThat(BinarySearch.isIncludedInList(integer, testList), CoreMatchers.equalTo(true));
+        });
+    }
+
+    @Test
+    public void isIncludedInListFalse() throws Exception {
+        final List<Integer> falseTestList = testList.stream().map((integer) -> integer * 100).collect(Collectors.toList());
+        falseTestList.forEach((integer) -> {
+            Assert.assertThat(BinarySearch.isIncludedInList(integer, testList), CoreMatchers.equalTo(false));
+        });
+    }
+}
