ae6d1667c294d524cd12f9f21eaf8d2dcc4c1e87
adam sarenski <asarenski@zipcar.com>
2018-02-03 23:48:39 -0500

    added fixture for DicePairGraph

---
+++ src/test/java/Fixtures/DicePairGraphFixture.java
@@ -0,0 +1,40 @@
+package Fixtures;
+
+import com.google.common.collect.Lists;
+import dice.Tuple;
+
+import java.util.List;
+
+public class DicePairGraphFixture {
+    public static List<Tuple> combinationsFor2() {
+        return Lists.newArrayList(
+                new Tuple<>(1, 1)
+        );
+    }
+
+    public static List<Tuple> combinationsFor3() {
+        return Lists.newArrayList(
+                new Tuple<>(1, 2),
+                new Tuple<>(2, 1)
+        );
+    }
+
+    public static List<Tuple> combinationsFor4() {
+        return Lists.newArrayList(
+                new Tuple<>(1, 3),
+                new Tuple<>(2, 2),
+                new Tuple<>(3, 1)
+        );
+    }
+
+    public static List<Tuple> combinationsFor7() {
+        return Lists.newArrayList(
+                new Tuple<>(1, 6),
+                new Tuple<>(2, 5),
+                new Tuple<>(3, 4),
+                new Tuple<>(4, 3),
+                new Tuple<>(5, 2),
+                new Tuple<>(6, 1)
+        );
+    }
+}
--- src/test/java/dice/testDicePairGraph.java
+++ src/test/java/dice/testDicePairGraph.java
@@ -1,5 +1,6 @@
 package dice;
 
+import Fixtures.DicePairGraphFixture;
 import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,33 +14,10 @@ public class testDicePairGraph {
         DicePair dice = new DicePair(6);
         DicePairGraph dicePairGraph = new DicePairGraph(dice);
 
-        final List<Tuple> expectedFor2 = Lists.newArrayList(
-                new Tuple<>(1, 1)
-        );
-        Assert.assertEquals(dicePairGraph.generateCombinationsForValue(2), expectedFor2);
-
-        final List<Tuple> expectedFor3 = Lists.newArrayList(
-                new Tuple<>(1, 2),
-                new Tuple<>(2, 1)
-        );
-        Assert.assertEquals(dicePairGraph.generateCombinationsForValue(3), expectedFor3);
-
-        final List<Tuple> expectedFor4 = Lists.newArrayList(
-                new Tuple<>(1, 3),
-                new Tuple<>(2, 2),
-                new Tuple<>(3, 1)
-        );
-        Assert.assertEquals(dicePairGraph.generateCombinationsForValue(4), expectedFor4);
-
-        final List<Tuple> expectedFor7 = Lists.newArrayList(
-                new Tuple<>(1, 6),
-                new Tuple<>(2, 5),
-                new Tuple<>(3, 4),
-                new Tuple<>(4, 3),
-                new Tuple<>(5, 2),
-                new Tuple<>(6, 1)
-        );
-        Assert.assertEquals(dicePairGraph.generateCombinationsForValue(7), expectedFor7);
+        Assert.assertEquals(dicePairGraph.generateCombinationsForValue(2), DicePairGraphFixture.combinationsFor2());
+        Assert.assertEquals(dicePairGraph.generateCombinationsForValue(3), DicePairGraphFixture.combinationsFor3());
+        Assert.assertEquals(dicePairGraph.generateCombinationsForValue(4), DicePairGraphFixture.combinationsFor4());
+        Assert.assertEquals(dicePairGraph.generateCombinationsForValue(7), DicePairGraphFixture.combinationsFor7());
     }
 
     @Test
